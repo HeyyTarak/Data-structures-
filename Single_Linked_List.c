1cc01680d3526b54df6b774593eccee1226eee1c
HeyyTarak <121956334+HeyyTarak@users.noreply.github.com>
2024-08-27 22:32:26 +0530

    Update Single_Linked_List.c

--- Single_Linked_List.c
+++ Single_Linked_List.c
@@ -5,8 +5,6 @@ struct node
 {
     int data;
     struct node *link;
-    
-    
 };
 
 void insertionAtBegin(struct node **head,int value)
@@ -107,26 +105,31 @@ int main() {
     head->link=NULL;
     
     //you can also make dynamic with scanf
+    printf("Insertion at Begining: \n");
     insertionAtBegin(&head,10);
     insertionAtBegin(&head,20);
     insertionAtBegin(&head,30);
     count_nodes(head);
     display(head);
-    
+
+    printf("Insertion at Ending: \n");
     insertionAtEnd(head,60);
     insertionAtEnd(head,70);
     insertionAtEnd(head,80);
     count_nodes(head);
     display(head);
-    
+
+    printf("insertion at specific: \n");
     insertionAtCertain(head,25,45);
     count_nodes(head);
     display(head);
-    
+
+    printf("Deletion at Begin: \n");
     deletionAtBegin(&head);
     count_nodes(head);
     display(head);
-    
+
+    printf("Deletion at End: \n");
     deletionAtEnd(head);
     count_nodes(head);
     display(head);
