7848877c4d1fa6147fbfb1e1e4791054be5f0c5c
HeyyTarak <121956334+HeyyTarak@users.noreply.github.com>
2024-12-10 19:57:05 +0530

    Doubly_Linked_List.c

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -1,49 +1,62 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-struct node
-{
+
+struct node {
     int data;
     struct node *next;
     struct node *prev;
 };
 
-void insertionAtBegin(struct node **head,int value)
-{
-    struct node *ptr=*head;
-    struct node *temp=malloc(sizeof(struct node));
-    temp->data=value;
-    temp->next=ptr;
-    temp->prev=NULL;
-    ptr->prev=temp;
-    *head=temp;
-    
+void insertionAtBegin(struct node **head, int value) {
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = value;
+    temp->next = *head;
+    temp->prev = NULL;
+    if (*head != NULL) {
+        (*head)->prev = temp;
+    }
+    *head = temp;
 }
 
-void insertionAtEnd(struct node *head,int value)
-{
-    struct node *ptr=head;
-    struct node *temp=malloc(sizeof(struct node));
-    temp->data=value;
-    while(ptr->next!=NULL)
-        ptr=ptr->next;
-    temp->next=NULL;
-    ptr->next=temp;
-    temp->prev=ptr;
+void insertionAtEnd(struct node **head, int value) {
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if (*head == NULL) {
+        temp->prev = NULL;
+        *head = temp;
+        return;
+    }
+
+    struct node *ptr = *head;
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+    temp->prev = ptr;
 }
 
-void insertionAtCertain(struct node *head, int value, int position) 
-{
+void insertionAtCertain(struct node *head, int value, int position) {
+    if (position <= 0) {
+        printf("Invalid position!\n");
+        return;
+    }
+
     struct node *ptr = head;
     struct node *temp = malloc(sizeof(struct node));
     temp->data = value;
 
-    // Traverse the list until reaching the desired position or the end
     for (int i = 1; i < position - 1 && ptr != NULL; i++) {
         ptr = ptr->next;
     }
 
-    // Insert the new node
+    if (ptr == NULL) {
+        printf("Position out of bounds!\n");
+        free(temp);
+        return;
+    }
+
     temp->next = ptr->next;
     temp->prev = ptr;
     if (ptr->next != NULL) {
@@ -52,81 +65,93 @@ void insertionAtCertain(struct node *head, int value, int position)
     ptr->next = temp;
 }
 
-void deletionAtBegin(struct node **head)
-{   
-    struct node *ptr=*head;
-    *head=ptr->next;
-    ptr->next->prev=NULL;
+void deletionAtBegin(struct node **head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct node *ptr = *head;
+    *head = ptr->next;
+    if (*head != NULL) {
+        (*head)->prev = NULL;
+    }
     free(ptr);
 }
 
-void deletionAtEnd(struct node *head)
-{
-    struct node *ptr = head;
-    while(ptr->next!=NULL)
-    {
-        ptr=ptr->next;   
+void deletionAtEnd(struct node **head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
     }
-    ptr->prev->next=NULL;
-    free(ptr); 
+
+    struct node *ptr = *head;
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+
+    if (ptr->prev != NULL) {
+        ptr->prev->next = NULL;
+    } else {
+        *head = NULL;  // Only one element in the list
+    }
+    free(ptr);
 }
 
-void display(struct node *head)
-{
-    if(head==NULL)
-        printf("No content");
-    struct node *ptr=head;
+void display(struct node *head) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct node *ptr = head;
     printf("Forward: ");
-    while (ptr!=NULL)
-    {
+    while (ptr != NULL) {
         printf("%d -> ", ptr->data);
-        if(ptr->next==NULL)
-        {
-            break;
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+
+    // Print backward
+    if (head != NULL) {
+        while (head->next != NULL) {
+            head = head->next;
         }
-        ptr=ptr->next;
     }
-    printf("NULL \n");
-    
     printf("Backward: ");
-    while(ptr!=NULL)
-    {
-        printf("%d -> ",ptr->data);
-        ptr=ptr->prev;
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->prev;
     }
-    printf("NULL \n");
-    
+    printf("NULL\n");
 }
+
 int main() {
-    struct node *head;
-    head=malloc(sizeof(struct node));
-    printf("Enter elements into your node 1: ");
-    scanf("%d",&head->data);
-    head->next=NULL;
-    head->prev=NULL;
-    
-    //you can also make dynamic with scanf
-    printf("Insertion at Begining: \n");
-    insertionAtBegin(&head,10);
-    insertionAtBegin(&head,20);
-    insertionAtBegin(&head,30);
+    struct node *head = NULL;
+
+    printf("Insertion at Beginning:\n");
+    insertionAtBegin(&head, 10);
+    insertionAtBegin(&head, 20);
+    insertionAtBegin(&head, 30);
     display(head);
-    
-    printf("Insertion at Ending: \n");
-    insertionAtEnd(head,60);
-    insertionAtEnd(head,70);
-    insertionAtEnd(head,80);
+
+    printf("Insertion at End:\n");
+    insertionAtEnd(&head, 60);
+    insertionAtEnd(&head, 70);
+    insertionAtEnd(&head, 80);
     display(head);
-    
-    printf("insertion at specific: \n");
-    insertionAtCertain(head,25,45);
+
+    printf("Insertion at Specific Position:\n");
+    insertionAtCertain(head, 25, 3);
     display(head);
-    
-    printf("Deletion at Begin: \n");
+
+    printf("Deletion at Beginning:\n");
     deletionAtBegin(&head);
     display(head);
-    
-    printf("Deletion at End: \n");
-    deletionAtEnd(head);
+
+    printf("Deletion at End:\n");
+    deletionAtEnd(&head);
     display(head);
+
+    return 0;
 }
